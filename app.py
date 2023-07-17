8e0655ea626c0de02af59a4b4a7339400377114e
YAMANOE <139040160+YAMANOE@users.noreply.github.com>
2023-07-17 22:21:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,29 +19,7 @@ while i<=3:
     print(x)
 
 # --------------------------------------------
-#Q2
 
-z=2
-c=1
-m=0
-while c<=3:
-    a=int(input("inter the number : "))
-    c+=1
-    while m<=a:
-        if a%2==0:
-            print("even")
-            break
-        else :
-            print ("odd")
-            break
-    while z<=a:
-      if a%z==0:
-        print("not prime")
-        z+=1
-        break
-else : 
-   print("prime ")
-print("llll")
    
 
 
