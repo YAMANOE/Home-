e91fac425e331915560bd18519c2609efaaf5ca6
YAMANOE <139040160+YAMANOE@users.noreply.github.com>
2023-07-17 20:30:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-i=1
-
-#newstring=""
+f=1
+n=0
+i=0
 while i<=3:
-    string=input("tnter the name : ")
-    i=i+1 
-    newstring=len(string)
+    x=input("enter the name : ")
+    i+=1
+    y=len(x)
+    while f<=y:
+        if x[n]=="l" and "u":
+            x=x[0:n]+'*'+x[n+1:]
+            n+=1
+            f+=1    
+        print(x)
+
     
